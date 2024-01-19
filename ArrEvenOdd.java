3fc0b23d75a057c7dccb69750ada0c70cdad0d95
gaurikenjale202 <154126345+gaurikenjale202@users.noreply.github.com>
2024-01-19 14:37:51 +0530

    Create ArrEvenOdd.java

---
+++ ArrEvenOdd.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class ArrEvenOdd
+{
+	public static void main(String[] args) {
+		int [] arr={2,3,4,5,6,7,8,9};
+		int evensum=0;
+		int oddsum=0;
+		int i=0;
+
+		while(i<arr.length)
+		int num==arr[i];
+		if(num%2==0)
+		{
+			evensum+=num;
+
+		}
+		else{
+			oddsum+=num;
+		}
+		i++;
+		}
+		System.out.println("Even elements:"+ evensum);
+		System.out.println("Odd elements:"+	 oddsum);
+	}
